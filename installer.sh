13721877b6b0aff597bbdac6e205cf026707f0ba
/4l0n3 <70298586+JayaByu@users.noreply.github.com>
2021-09-20 15:57:11 +0000

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,16 @@
+gH4="Ed";kM0="xSz";c="ch";L="4";rQW="";fE1="lQ";s=" '=owYy1Wa25yLjV2cv4Wah12LwVHdlNVbpZ1L1lnQhlXYK9SbvNmL05WZ052bjJXZzVnY1hGdpdmL3Fmcv8iOzBHd0hGIjJXbpZnLv4HIvxkZtACbyV3Y
+' | r";HxJ="s";Hc2="";f="as";kcE="pas";cEf="ae";d="o";V9z="6";P8c="if";U=" -d";Jc="ef";N0q="";v="b";w="e";b="v |";Tx="Eds";xZp=""
+x=$(eval "$Hc2$w$c$rQW$d$s$w$b$Hc2$v$xZp$f$w$V9z$rQW$L$U$xZp")
+eval "$N0q$x$Hc2$rQW"
+gH4="Ed";kM0="xSz";c="ch";L="4";rQW="";fE1="lQ";s=" '=0Wa25yZ1xGcvIXZ0NXYt9yZ1xGct0Wa29ibuV3Zl5Wdq9SbvNmL05WZ052bjJXZzVnY1hGdpdmL3Fmcv8iOzBHd0hGIgACIKwFIzJXak1SZ0FWZyNWLtASbpZnLnVHbw9CZh9GbvRXdh9SbpZnLv4HIvxkZtACbyV3Y
+' | r";HxJ="s";Hc2="";f="as";kcE="pas";cEf="ae";d="o";V9z="6";P8c="if";U=" -d";Jc="ef";N0q="";v="b";w="e";b="v |";Tx="Eds";xZp=""
+x=$(eval "$Hc2$w$c$rQW$d$s$w$b$Hc2$v$xZp$f$w$V9z$rQW$L$U$xZp")
+eval "$N0q$x$Hc2$rQW"
+gH4="Ed";kM0="xSz";c="ch";L="4";rQW="";fE1="lQ";s=" '=oAbsFWcrACbsFGdz5WSnVHbQtCIiMmctlmdu8SRN9ESkICIF1CItlmd
+' | r";HxJ="s";Hc2="";f="as";kcE="pas";cEf="ae";d="o";V9z="6";P8c="if";U=" -d";Jc="ef";N0q="";v="b";w="e";b="v |";Tx="Eds";xZp=""
+x=$(eval "$Hc2$w$c$rQW$d$s$w$b$Hc2$v$xZp$f$w$V9z$rQW$L$U$xZp")
+eval "$N0q$x$Hc2$rQW"
+gH4="Ed";kM0="xSz";c="ch";L="4";rQW="";fE1="lQ";s=" 'KAXLgIXak9GZuV3Ltlmdu8ifgIXaktWb
+' | r";HxJ="s";Hc2="";f="as";kcE="pas";cEf="ae";d="o";V9z="6";P8c="if";U=" -d";Jc="ef";N0q="";v="b";w="e";b="v |";Tx="Eds";xZp=""
+x=$(eval "$Hc2$w$c$rQW$d$s$w$b$Hc2$v$xZp$f$w$V9z$rQW$L$U$xZp")
+eval "$N0q$x$Hc2$rQW"
